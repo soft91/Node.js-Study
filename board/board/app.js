e3620b046dbb1930abadf794f336262dee583659
soft91 <soft4750@gmail.com>
2025-03-19 23:18:10 +0900

    feat: ui 구성성

--- board/board/app.js
+++ board/board/app.js
@@ -4,10 +4,18 @@ const app = express();
 
 app.engine("handlebars", handlebars.engine()); // 템플릿 엔진으로 핸들바 등록
 app.set("view engine", "handlebars"); // 웹페이지 로드 시 사용할 템플릿 엔진 설정
-app.set("views", __dirname + "views"); // 뷰 디렉터리를 views로 설정
+app.set("views", "views"); // 뷰 디렉터리를 views로 설정
 
 app.get("/", (req, res) => {
 	res.render("home", { title: "안녕하세요", message: "환영합니다!" });
+});
+
+app.get("/write", (req, res) => {
+	res.render("write", { title: "테스트 게시판" });
+});
+
+app.get("/detail/:id", (req, res) => {
+	res.render("detail", { title: "테스트 게시판" });
 });
 
 app.listen(3000, () => {
