806d6dfc4df316fc430571a6cd7373b5b4f5c053
soft91 <soft4750@gmail.com>
2025-08-03 18:02:18 +0900

    Initial commit: NestJS chat application with modern UI

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,19 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class ChatGateway {
+  @WebSocketServer() server: Server;
+
+  @SubscribeMessage('message')
+  handleMessage(socket: Socket, data: any): void {
+    this.server.emit(
+      'message',
+      `client-${socket.id.substring(0, 4)} says ${data}`,
+    );
+  }
+}
